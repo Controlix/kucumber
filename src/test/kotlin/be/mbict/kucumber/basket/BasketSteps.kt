694fadae9b4f5c5adf0a361d9de0ffde0093fb4e
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2023-01-28 12:55:00 +0100

    Implement all steps for first scenario

--- src/test/kotlin/be/mbict/kucumber/basket/BasketSteps.kt
+++ src/test/kotlin/be/mbict/kucumber/basket/BasketSteps.kt
@@ -1,13 +1,30 @@
 package be.mbict.kucumber.basket
 
+import io.cucumber.java.PendingException
 import io.cucumber.java.en.Given
+import io.cucumber.java.en.Then
+import io.cucumber.java.en.When
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 
 class BasketSteps(val basket: Basket) {
 
-    @Given("I put {int} apples in my basket")
-    fun `I put some apples in my basket`(amount: Int) {
-        (0..amount).forEach {
+    private var result: Long? = null
+
+    @Given("I put {long} apples in my basket")
+    fun `I put some apples in my basket`(amount: Long) {
+        (1..amount).forEach {
             basket.save(Item(type = "apple"))
         }
+    }
+
+    @When("I count how many apples there are in the basket")
+    fun `I count how many apples there are in the basket`() {
+        result = basket.count()
+    }
+
+    @Then("I have {long} apples")
+    fun `I have apples`(amount: Long) {
+        assertThat(result).isEqualTo(amount)
     }
 }
