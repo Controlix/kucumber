d549a39cc18b8f37ae5d56d20e74099acf699aa4
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2023-01-28 19:33:07 +0100

    Replace java bootstrap class by kotlin object

--- src/test/kotlin/be/mbict/kucumber/KucumberTest.kt
+++ src/test/kotlin/be/mbict/kucumber/KucumberTest.kt
@@ -3,13 +3,12 @@ package be.mbict.kucumber
 import io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME
 import io.cucumber.java.BeforeAll
 import io.cucumber.spring.CucumberContextConfiguration
-import jakarta.annotation.PostConstruct
 import org.junit.platform.suite.api.ConfigurationParameter
 import org.junit.platform.suite.api.IncludeEngines
 import org.junit.platform.suite.api.SelectClasspathResource
 import org.junit.platform.suite.api.Suite
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.utility.DockerImageName
@@ -20,20 +19,20 @@ import org.testcontainers.utility.DockerImageName
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 class KucumberTest
 
-//@CucumberContextConfiguration
-//@SpringBootTest(webEnvironment = RANDOM_PORT)
-//@ActiveProfiles("test")
-//class KucumberBootstrap {
-//
-//    companion object {
-//        @org.junit.jupiter.api.BeforeAll
-//        @JvmStatic
-//        fun startContainer() {
-//            MySQLContainer(DockerImageName.parse("mysql").withTag("8.0.32"))
-//                .withDatabaseName("basket-db")
-//                .withUsername("obiwan")
-//                .withPassword("s3cr3t")
-//                .start()
-//        }
-//    }
-//}
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+object KucumberKotlinBootstrap {
+    @BeforeAll
+    @JvmStatic
+    fun startContainer() {
+        val mySql = MySQLContainer(DockerImageName.parse("mysql").withTag("8.0.32"))
+            .withDatabaseName("basket-db")
+            .withUsername("obiwan")
+            .withPassword("s3cr3t")
+            .apply { start() }
+
+        println(mySql.jdbcUrl)
+        System.setProperty("spring.datasource.url", mySql.jdbcUrl)
+    }
+}
